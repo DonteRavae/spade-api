ff0ba1bd78dac2d19cdcc91a03523c8f09055d2a
Donte Littlejohn <donteravae@gmail.com>
2024-04-18 14:43:01 -0400

    added community db shemas

---
+++ src/db/community.schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE user_profiles(
+    id VARCHAR(100) PRIMARY KEY, 
+    avatar TEXT, 
+    username VARCHAR(255) NOT NULL UNIQUE,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE expression_posts(
+    id VARCHAR(255) PRIMARY KEY,
+    title TEXT NOT NULL,
+    subtitle TEXT,
+    content_type ENUM('text', 'image') NOT NULL,
+    content_value TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE replies (
+    id VARCHAR(100) PRIMARY KEY,
+    author VARCHAR(100) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (author) REFERENCES user_profiles(id) ON DELETE CASCADE
+);
+
+CREATE TABLE likes (
+    parent_id VARCHAR(255) NOT NULL,
+    author VARCHAR(100) NOT NULL
+);
