3979d83b4011f83c5b04970129e3001ad6f8d811
Donte Littlejohn <donteravae@gmail.com>
2024-04-18 22:53:57 -0400

    updated community db schema

--- src/db/community.schema.sql
+++ src/db/community.schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_profiles(
-    id VARCHAR(100) PRIMARY KEY, 
-    avatar TEXT, 
+    id VARCHAR(100) PRIMARY KEY,
+    avatar TEXT,
     username VARCHAR(255) NOT NULL UNIQUE,
     last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
@@ -9,10 +9,12 @@ CREATE TABLE expression_posts(
     id VARCHAR(255) PRIMARY KEY,
     title TEXT NOT NULL,
     subtitle TEXT,
+    author VARCHAR(100) NOT NULL,
     content_type ENUM('text', 'image') NOT NULL,
     content_value TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (author) REFERENCES user_profiles(id) ON DELETE CASCADE
 );
 
 CREATE TABLE replies (
