9e16041cb70562084c775970570f054a547c91e4
Donte Littlejohn <donteravae@gmail.com>
2024-05-04 20:55:04 -0400

    converted result error to string

--- src/auth/models/password.rs
+++ src/auth/models/password.rs
@@ -1,15 +1,12 @@
 use argon2::{password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier};
-use async_graphql::{Error, ErrorExtensions};
 use fancy_regex::Regex;
 use rand::rngs::OsRng;
-
-use crate::auth::AuthError;
 
 #[derive(Debug)]
 pub struct Password(String);
 
 impl Password {
-    pub fn parse(password: String) -> Result<Self, Error> {
+    pub fn parse(password: String) -> Result<Self, String> {
         let password_validation_test =
             Regex::new(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$").unwrap();
         match password_validation_test.is_match(&password) {
@@ -17,37 +14,33 @@ impl Password {
                 if result {
                     Ok(Password(password))
                 } else {
-                    Err(
-                        AuthError::BadRequest("Please enter a valid email or password".to_string())
-                            .extend_with(|_, e| e.set("code", 400)),
-                    )
+                    Err("Please enter a valid email or password".to_string())
                 }
             }
-            Err(_) => Err(AuthError::ServerError(
-                "There seems to be an error on our end. Please try again.".to_string(),
-            )
-            .extend_with(|_, e| e.set("code", 500))),
+            Err(_) => Err("There seems to be an error on our end. Please try again.".to_string()),
         }
     }
 
-    pub fn verify(&self, hash: &str) -> Result<(), Error> {
+    pub fn verify(&self, hash: &str) -> Result<(), String> {
         if Argon2::default()
             .verify_password(self.0.as_bytes(), &PasswordHash::new(hash).unwrap())
             .is_err()
         {
-            return Err(AuthError::VerificationError(
-                "Please enter a valid email or password.".to_string(),
-            )
-            .extend_with(|_, e| e.set("code", 400)));
+            return Err("Please enter a valid email or password.".to_string());
         }
 
         Ok(())
     }
 
-    pub fn hash(&self) -> Result<String, Error> {
+    pub fn hash(&self) -> Result<String, String> {
         let argon2 = Argon2::default();
         let salt = SaltString::generate(&mut OsRng);
 
-        Ok(argon2.hash_password(self.0.as_bytes(), &salt)?.to_string())
+        let Ok(hashed_password) = argon2.hash_password(self.0.as_bytes(), &salt) else {
+            eprintln!("PASSWORD_ERROR: There was an error hashing password.");
+            return Err("Server error. Please try again.".to_string());
+        };
+
+        Ok(hashed_password.to_string())
     }
 }
